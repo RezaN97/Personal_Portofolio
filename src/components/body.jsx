6288ce17bc30b26595ce532d1fa56632b73943c7
RezaN97 <rezanurit16@gmail.com>
2025-09-19 23:44:33 +0700

    update UI desktop

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -68,38 +68,36 @@ export const HeaderText = () => {
          
             </div>
         {/* desktop */}
-            <div  className="max-sm:hidden md:block flex justify-center w-full h-80 mt-10 font-medium text-10 ">
-                <h1 className='text-6xl'>Hi, I'am Reza</h1>
-                <p className="w-[80vw] ml-4 mt-10 mb-5 text-left">I'm fullstack developer who builds web
+            <div  className="max-sm:hidden flex flex-col w-full h-80 mt-20 font-medium text-10 ">
+                <h1 className='text-6xl flex mx-4'>Hi, I'am Reza</h1>
+                <p className="w-[80vw] ml-4 mt-10 text-2xl font-light mb-5 text-left">I'm fullstack developer who builds web
                      applications from user interface design to backend logic.
                       With experience in Express.js, MySQL, and React,
                      I focus on creating scalable and secure solutions.</p>
               {/* icon skill */}
-              <div className="flex gap-5">
-                <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>Javascript</h1>
+                <div className="flex gap-5 mx-3 font-mono">
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>Javascript</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>Tailwind</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>React</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>Express</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>Next Js</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>MySQL</h1>
+                    </div>
+                    <div className="w-25 h-10  bg-black rounded-xl p-2">
+                        <h1>Rest API</h1>
+                    </div>
                 </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>Tailwind</h1>
-                </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>React</h1>
-                </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>Express Js</h1>
-                </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>Next Js</h1>
-                </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>MySQL</h1>
-                </div>
-                  <div className="w-25 h-10  bg-amber-600 rounded-xl p-2">
-                    <h1>Rest API</h1>
-                </div>
-                
-                
-              </div>
             </div>
 
         </>
@@ -125,7 +123,7 @@ export const Portofolio = () => {
                         </div>
                         {/* text and logo */}
                         <div className="bg-orange-400 text-left w-[80vw] h-[40vh]">
-                                <p className='pt-2 pl-5 font-semibold'>This app todo list with feature: <br />
+                                <p className='pt-2 pl-5 font-semibold'>This apps todo list with feature: <br />
                                 </p>
                                 <p className=' pl-5 mb-2 text-[10px] '>
                                     1.Display your personal information like username and jabatan/ jobdesk <br />
@@ -198,42 +196,40 @@ export const Portofolio = () => {
                 {/* CONTENT */}
                 <div className="w-full h-auto ">
                     {/* content 1 */}
-                    <div className="flex justify-center mt-2 mb-15 w-full">
+                    <div className="flex justify-center mt-10 mb-15 w-full">
                         {/* picture */}
-                        <div className="bg-orange-500 p-10 w-[40vw] h-[40vh] flex justify-center items-center">
-                            <img className='w-60 pb-9' src={ssApp1} alt="App TodoList" />
+                        <div className="bg-orange-500  p-10 w-[40vw] h-[40vh] flex justify-center items-center">
+                            <img className='w-60 pb-9 rounded-t-lg' src={ssApp1} alt="App TodoList" />
                         
                         </div>
                         {/* text and logo */}
                         <div className="bg-orange-400 text-left w-[80vw] h-[40vh]">
-                                <p className='pt-2 pl-5 font-semibold'>This app todo list with feature: <br />
+                                <h2 className='text-2xl font-bold ml-8 underline'>Todo List Apps</h2>
+                                <p className='pt-2 pl-5 font-semibold'>This app have some feature: <br />
                                 </p>
                                 <p className='pt-2 pl-5 mb-3 text-[14px]'>
-                                    1.Display your personal information like username and jabatan/ jobdesk <br />
+                                    1.Display your personal information like username and job position/ jobdesk <br />
                                     2. Display realtime timezone at right side <br />
                                     3. Set your task inside it, and you can set that priority of your task off course
 
                                 </p>
                         {/* button */}
-                        <button className='ml-20 bg-orange-600 w-20 h-10 rounded'><a className='text-white' href="https://github.com/RezaN97/Simple-TodoListAPP">Github</a></button>
+                        <button className='ml-20 bg-orange-600 w-20 h-10 hover:bg-orange-800 rounded font-semibold'><a className='text-white' href="https://github.com/RezaN97/Simple-TodoListAPP">Github</a></button>
                         </div>
                     </div>
                     {/* content 2 */}
                         <div className=" flex justify-center mt-4 w-full h-auto">
                             {/* picture */}
                             <div className="bg-gray-900 p-1 w-[42vw] h-[58vh] flex justify-center items-center">
-                                <img className='w-auto h-60  ' src={ssApp2} alt="App TodoList" />
+                                <img className='w-auto h-full py-5  ' src={ssApp2} alt="App TodoList" />
                             
                             </div>
                             {/* text and logo */}
                             <div className=" bg-gray-800 text-left w-[80vw] h-[58vh]">
                                     
-                                    <p className='pl-5 pt-1 font-semibold'>
-                                    Welcome to Chill Movie
-                                    Your personal gateway to the world of entertainment—where movies meet simplicity and control.
-                                    </p>
-                                    <p className='ml-8 text-[14px]'>
-                                    
+                                    <p className='pl-5 pt-1 text-2xl font-bold underline'> Chill Movies - Streaming Movie</p>
+                                    <p className='ml-8 text-[14px] w-180'>
+                                  <strong> Feature:</strong>   <br />
                                     1 Login System
                                     Secure access with personalized login. Whether you're a casual viewer or an admin, your experience starts with a smooth sign-in.
                                     <br />
@@ -256,8 +252,8 @@ export const Portofolio = () => {
                                     </p>
                             {/* button */}
                             <div className="flex mt-3">
-                                <button className='ml-10 bg-red-600 w-20 h-10 rounded'><a className='font-bold' href="https://github.com/RezaN97/Chill-Movie/tree/study">Github</a></button>
-                                <button className='ml-10 bg-red-600 w-20 h-10 rounded'><a className='font-bold' href="https://chill-movie-six.vercel.app/">Visit</a></button>
+                                <button className='ml-10 bg-red-600 hover:bg-red-800 w-20 h-10 rounded'><a className='font-bold' href="https://github.com/RezaN97/Chill-Movie/tree/study">Github</a></button>
+                                <button className='ml-10 bg-red-600 hover:bg-red-800 w-20 h-10 rounded'><a className='font-bold' href="https://chill-movie-six.vercel.app/">Visit</a></button>
 
                             </div>
                         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,8 @@ const Navbar = () => {
 
 
 export const BgNavbar = () => {
-   const [showModal, setShowModal] = useState(false);
+    // setting for display contact modal
+   const [showModal, setShowModal] = useState(false); 
  
     return (
         <>
@@ -37,10 +38,10 @@ export const BgNavbar = () => {
                     </ul>
          {/* Modal */}
       {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black opacity-90 flex items-center justify-center z-50">
           <div className="bg-white text-black p-6 rounded-lg shadow-lg w-80">
             <h2 className="text-xl font-bold mb-2">Contact Person</h2>
-            <p className="mb-4">📞 WhatsApp: +62 </p>
+            <p className="mb-4">📞 WhatsApp: +62 8234459702 </p>
             <p className="mb-4">📧 Email: rezanurit16@gmail.com</p>
             <button
               className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
